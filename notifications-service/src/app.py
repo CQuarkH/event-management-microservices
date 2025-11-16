acbb782953db1b403874aa474b863fd11d3681a1
CQuarkH <currihuile@gmail.com>
2025-11-16 16:25:49 -0300

    chore(notifications): add main.py and reorganize project structure

---
+++ notifications-service/src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+if __name__ == '__main__':
+    app.run(
+        debug=app.config['DEBUG'], 
+        port=app.config['NOTIFICATIONS_PORT'],
+        host='0.0.0.0'
+    )
